91b76919eb6f5e7faa61ee3473a39335c7660e1a
Erik Campobadal <soc@erik.cat>
2017-02-27 00:52:36 +0100

    Tweaks

--- src/Injectors/user.php
+++ src/Injectors/user.php
@@ -4,6 +4,7 @@ return [
     [
         'text'  => __('laralum_notifications::general.my_notifications'),
         'url'   => route('laralum::notifications.index'),
+        'counter' => Laralum\Users\Models\User::findOrFail(Auth::id())->unreadNotifications->count(),
     ],
     [
         'text'  => __('laralum_notifications::general.create_notification'),
